cb9ad2f6d7e68a101d1faeddf7e1bbef92d453c4
morphZ <123morph@gmail.com>
2015-02-08 21:21:22 +0100

    Changed data structure to one-dimensional hash per trip.

--- scrape-kayak.js
+++ scrape-kayak.js
@@ -19,80 +19,34 @@ page.open(url, function(status) {
     }
     else
     {
-//        console.log("Page loaded");
-
         page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
             page.evaluate(function() {
                 $( "#fs_stops_1_input" ).click();
                 $( "#fs_stops_2_input" ).click();
             });
 
-//            console.log("Waiting 10 secs");
             setTimeout(function() {
-//                console.log("Grabbing Flight data");
                 var strResultsPJS = page.evaluate(function() {
-                    var strResults = [];
                     var objRes = [];
                     $( ".resultrow.flightresult" ).slice(0, 4).each( function(index,element) {
-                        strResults.push(
-                            $( element ).find(".results_price").text().replace(" ", "") +
-                            " | " +
-                            $.trim($( element ).find(".singleleg0").find(".airport").eq(0).text()) +
-                            "->" +
-                            $.trim($( element ).find(".singleleg0").find(".airport").eq(1).text()) +
-                            " " +
-                            $.trim($( element ).find(".singleleg0").find(".flightTimeDeparture").text()) +
-                            "->" +
-                            $.trim($( element ).find(".singleleg0").find(".flightTimeArrival").text()) +
-                            " (" +
-                            $.trim($( element ).find(".singleleg0").find(".stopsLayovers").text()) +
-                            ") | " +
-
-                            $.trim($( element ).find(".singleleg1").find(".airport").eq(0).text()) +
-                            "->" +
-                            $.trim($( element ).find(".singleleg1").find(".airport").eq(1).text()) +
-                            " " +
-                            $.trim($( element ).find(".singleleg1").find(".flightTimeDeparture").text()) +
-                            "->" +
-                            $.trim($( element ).find(".singleleg1").find(".flightTimeArrival").text()) +
-                            " (" +
-                            $.trim($( element ).find(".singleleg1").find(".stopsLayovers").text()) +
-                            ") | " +
-                            $.trim($( element ).find(".airlineName").text()).replace(/\r?\n|\r/g, "")
-                        );
-
                         var item = {};
-                        item["orig"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(0).text());
-                        item["dest"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(1).text());
-                        item["dep_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeDeparture").text());
-                        item["arr_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeArrival").text());
-                        item["stops"] = $.trim($( element ).find(".singleleg0").find(".stopsLayovers").text());
-
-                        var fluege = {};
-                        fluege["price"] = $( element ).find(".results_price").text().replace(" ", "");
-                        fluege["airline"] = $.trim($( element ).find(".airlineName").text()).replace(/\r?\n|\r/g, "");
-                        fluege["to"] = item;
-
-                        item = {};
-                        item["orig"] = $.trim($( element ).find(".singleleg1").find(".airport").eq(0).text());
-                        item["dest"] = $.trim($( element ).find(".singleleg1").find(".airport").eq(1).text());
-                        item["dep_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeDeparture").text());
-                        item["arr_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeArrival").text());
-                        item["stops"] = $.trim($( element ).find(".singleleg1").find(".stopsLayovers").text());
-                        fluege["from"] = item;
-
-                        objRes.push(fluege);
+                        item["origin"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(0).text());
+                        item["destination"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(1).text());
+                        item["price"] = $( element ).find(".results_price").text().replace(" ", "");
+                        item["airline"] = $.trim($( element ).find(".airlineName").text()).replace(/\r?\n|\r/g, "");
+                        item["outbound_dep_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeDeparture").text());
+                        item["outbound_arr_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeArrival").text());
+                        item["outbound_stops"] = $.trim($( element ).find(".singleleg0").find(".stopsLayovers").text());
+                        item["return_dep_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeDeparture").text());
+                        item["return_arr_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeArrival").text());
+                        item["return_stops"] = $.trim($( element ).find(".singleleg1").find(".stopsLayovers").text());
+                        objRes.push(item);
                     });
 
-                    //return strResults;
                     return objRes;
                 });
 
-                //console.log("Ergebnisse vom " + new Date + ":");
-                //console.log("URL: " + url);
-                //strResultsPJS.forEach( function(elem) { console.log(elem); } );
                 console.log(JSON.stringify(strResultsPJS));
-
                 phantom.exit();
 
             }, 10000);
