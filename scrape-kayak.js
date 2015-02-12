7ca6cef6b256213cfd388c6c6860fea37da80015
morphZ <123morph@gmail.com>
2015-02-12 08:39:22 +0100

    Reinserted 10 sec timeout, to let headless browser finish loading.

--- scrape-kayak.js
+++ scrape-kayak.js
@@ -15,29 +15,33 @@ page.open(url, function(status) {
   }
   else {
     page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-      var strResultsPJS = page.evaluate(function() {
-        var objRes = [];
-        $( ".resultrow.flightresult" ).slice(0, 4).each( function(index,element){
-          var item = {};
-          item["origin"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(0).text());
-          item["destination"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(1).text());
-          item["price"] = $( element ).find(".results_price").text().replace(" ", "");
-          item["airline"] = $.trim($( element ).find(".airlineName").text()).replace(/\r?\n|\r/g, "");
-          item["outbound_dep_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeDeparture").text());
-          item["outbound_arr_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeArrival").text());
-          item["outbound_stops"] = $.trim($( element ).find(".singleleg0").find(".stopsLayovers").text());
-          item["return_dep_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeDeparture").text());
-          item["return_arr_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeArrival").text());
-          item["return_stops"] = $.trim($( element ).find(".singleleg1").find(".stopsLayovers").text());
-          objRes.push(item);
+      setTimeout(function() {
+
+        var strResultsPJS = page.evaluate(function() {
+          var objRes = [];
+          $( ".resultrow.flightresult" ).slice(0, 4).each( function(index,element){
+            var item = {};
+            item["origin"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(0).text());
+            item["destination"] = $.trim($( element ).find(".singleleg0").find(".airport").eq(1).text());
+            item["price"] = $( element ).find(".results_price").text().replace(" ", "");
+            item["airline"] = $.trim($( element ).find(".airlineName").text()).replace(/\r?\n|\r/g, "");
+            item["outbound_dep_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeDeparture").text());
+            item["outbound_arr_time"] = $.trim($( element ).find(".singleleg0").find(".flightTimeArrival").text());
+            item["outbound_stops"] = $.trim($( element ).find(".singleleg0").find(".stopsLayovers").text());
+            item["return_dep_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeDeparture").text());
+            item["return_arr_time"] = $.trim($( element ).find(".singleleg1").find(".flightTimeArrival").text());
+            item["return_stops"] = $.trim($( element ).find(".singleleg1").find(".stopsLayovers").text());
+            objRes.push(item);
+          });
+
+          return objRes;
         });
 
-        return objRes;
-      });
+        fs.write(outfile, JSON.stringify(strResultsPJS), 'w');
+        //console.log(JSON.stringify(strResultsPJS));
+        phantom.exit();
 
-      fs.write(outfile, JSON.stringify(strResultsPJS), 'w');
-      //console.log(JSON.stringify(strResultsPJS));
-      phantom.exit();
+      }, 10000);
     });
   }
 });
